42667da237409087321c4a267c84d563fdf93244
Alex <kayin.lo219@gmail.com>
2025-09-29 18:43:21 +0800

    update

--- nonJsCrawer.py
+++ nonJsCrawer.py
@@ -20,6 +20,52 @@ import time #延遲
 import logging
 import json
 import os
+import threading
+
+#function to init a json file with default value
+def initJsonInput()->dict:
+    today = time.strftime("%d-%m-%y", time.localtime())
+    yesterday = time.strftime("%d-%m-%y", time.localtime(time.time() - 24 * 60 * 60))
+    userInput = {
+        "startDate": yesterday,
+        "endDate": today,
+        "domain": {"hkdiscuss": True,
+                    "babykingdom": True},
+        "keyword": []
+    }
+
+    return userInput
+#read a json file and return as a dict
+def readJson(filePath:str)->dict:
+    with open(filePath, "r",encoding="utf-8") as f:
+            userInput = json.load(f)
+    return userInput
+#read user input variable from json file, return varible one by one
+def userInputVariable(filePath:str):
+    userInput = readJson(filePath)
+    startDate = userInput["startDate"]
+    endDate = userInput["endDate"]  
+    domain = userInput["domain"]
+    keyword = userInput["keyword"]
+    mode = userInput["mode"]
+    frequency = userInput["frequency"]
+    return startDate,endDate,domain,keyword,mode,frequency
+#function to print user input variable from json file
+def printInput(file_path:str):
+    startDate,endDate,domain,keyword,mode,frequency = userInputVariable(file_path)
+    print(f"scan start at: {startDate} to {endDate}")  
+    print(f"form the following domain: {domain}")
+    print(f"searching keywords: {keyword}")
+    print(f"operating mode: {mode}")
+    print(f"scan frequency: run script every {frequency} minutes")
+
+def writeJson(filePath:str,data:dict):
+    try:
+        with open(filePath, 'w',encoding="utf-8") as f:
+            json.dump(data, f, indent=4,ensure_ascii=False)  # indent for pretty-printing
+        print(f"JSON file successfully created at: {filePath}")
+    except IOError as e:
+        print(f"Error creating JSON file at {filePath}: {e}")
 
 def URLtoHTML(url)->str:
     time.sleep(1)#每次request間隔一秒，防止被server ban
@@ -82,7 +128,7 @@ def totalPage(html,element,classOrId,name)->str:
     pageSet = soup.find_all(element,attrs = {classOrId : re.compile(name)})
     for item in pageSet:
         if item.get_text()!="" and item.get_text()!=" ":
-            print(f"page test:{item.get_text()}")
+            #print(f"page test:{item.get_text()}")
             page = str(item.get_text()).replace(" ","")
             totalPageNumber = page[-1]
             logging.debug(page)           
@@ -98,10 +144,23 @@ def sample_babyKindom(tid):
     TotalcommentList = []
     postCreateDate = ""
     totalPageNumber = 1
+    url = f'https://www.baby-kingdom.com/forum.php?mod=viewthread&tid={tid}&extra=page%3D1&page={page}'
+    commentJson = {
+        
+            "title":"",
+            "tid": tid,
+            "date": "",
+            "totalPage": "",
+            "commentList": []
+        }
+    
+
     try:
         while haveNextPage == True:
             #目標網址，tid為主題編號，page為頁數
             url = f'https://www.baby-kingdom.com/forum.php?mod=viewthread&tid={tid}&extra=page%3D1&page={page}'
+            commentJson["commentList"].append({url:[]})
+            pos = 1
             html = URLtoHTML(url)
             title = titleString(html)
             date = DateString(html,"em","id","^auth")#更改此行的element,classOrId,name參數以取得正確的日期
@@ -116,18 +175,29 @@ def sample_babyKindom(tid):
             print(f"第{page}頁留言:")
             
             for i in commentPerPage:#顯示當前頁數的所有留言
-                print("\n")
-                print(i)
+                comment = str(i).replace("\n","")
+                commentJson["commentList"][page-1].update({pos:comment})
+                #commentJson["commentList"][url].append(i)
+                #print("\n")
+                #print(i)
+                pos +=1
             
             page += 1
             if page>int(totalPageNumber):#判斷是否有下一頁
                 haveNextPage = False
         
     except Exception as e:
-        print(f"這個主題共有{page}頁")
+        #print(f"這個主題共有{page}頁")
+        print(e)
     finally:
+        commentJson["title"] = title
+        commentJson["date"] = postCreateDate
+        commentJson["totalPage"] = totalPageNumber
         print(f"網址:{url}\n標題:{title}\n創建主題日期:{postCreateDate}\n頁數:{totalPageNumber}頁\n此主題共有留言{len(TotalcommentList)}則")#顯示主題資訊
-        print(f"完成主題{title}的爬取")
+        print(f"完成主題:{title}的爬取")
+        file_path = fr"C:\Users\Alex\ListeningTool\{title}.json"
+        writeJson(file_path,commentJson) 
+        print(commentJson)
 
 def sample_hkdiscuss(tid):
     page = 1
@@ -187,7 +257,7 @@ def search_babyKindom(keywordList:list)->list:
         resultList.extend(urlList)
     print(f"在Babykindom中，共找到{len(resultList)}個相關主題")
     return resultList
-#
+
 def search_by_keyword_babykindom(keywordList:list):
     resultList = search_babyKindom(keywordList)
     #print(len(resultList))
@@ -202,50 +272,25 @@ def search_by_keyword(kewordList:list,domain:str):
         case "hkdiscuss":
             print("not support yet!")
 
-#function to init a json file with default value
-def initJsonInput()->dict:
-    today = time.strftime("%d-%m-%y", time.localtime())
-    yesterday = time.strftime("%d-%m-%y", time.localtime(time.time() - 24 * 60 * 60))
-    userInput = {
-        "startDate": yesterday,
-        "endDate": today,
-        "domain": {"hkdiscuss": True,
-                    "babykingdom": True},
-        "keyword": []
-    }
 
-    return userInput
-#read a json file and return as a dict
-def readJson(filePath:str)->dict:
-    with open(filePath, "r",encoding="utf-8") as f:
-            userInput = json.load(f)
-    return userInput
-#read user input variable from json file, return varible one by one
-def userInputVariable(filePath:str):
-    userInput = readJson(filePath)
+#example for using userInput function
+#file_path = r"C:\Users\Alex\userInput.json"
+#printInput(file_path)
 
-    startDate = userInput["startDate"]
-    endDate = userInput["endDate"]  
-    domain = userInput["domain"]
-    keyword = userInput["keyword"]
-    mode = userInput["mode"]
-    frequency = userInput["frequency"]
-    return startDate,endDate,domain,keyword,mode,frequency
-#function to print user input variable from json file
-def printInput(file_path:str):
-    startDate,endDate,domain,keyword,mode,frequency = userInputVariable(file_path)
-    print(f"scan start at: {startDate} to {endDate}")  
-    print(f"form the following domain: {domain}")
-    print(f"searching keywords: {keyword}")
-    print(f"operating mode: {mode}")
-    print(f"scan frequency: run script every {frequency} minutes")
+sample_babyKindom("23814831")
 
-file_path = r"C:\Users\Alex\userInput.json"
-printInput(file_path)
 
+#########################################################test area:########################################################
 #example of searching by keyword   
 #keywordList = ['可樂','KFC']
 #search_by_keyword(keywordList,"babykingdom")
 
 #initInput = initJsonInput()
 #print(initInput)
+"""
+#example for using threading to run multiple thread
+t1 = threading.Thread(target=sample_babyKindom, args=("23294505",))
+t2 = threading.Thread(target=sample_babyKindom,args=("23527900",))
+t1.start()
+t2.start()
+"""
